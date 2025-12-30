3218adc8bcd1a62a690e3b13cb2c54d53a94d5e8
NelcifranMagalhaes <nelcifranpires@gmail.com>
2025-12-30 20:03:48 -0300

    Add Chartkick integration and enhance dashboard with financial insights

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,5 +7,26 @@ class DashboardController < ApplicationController
     @money_in_in_current_month = MoneyIn
                                       .where(money_date: Time.current.beginning_of_month..Time.current.end_of_month)
                                       .sum(:amount)
+
+    @money_out_by_category = MoneyOut
+                   .joins(:category)
+                   .where(money_date: Time.current.beginning_of_month..Time.current.end_of_month)
+                   .group("categories.name")
+                   .sum(:amount)
+
+    start_month = Time.current.beginning_of_month - 5.months
+
+    @money_out_by_month = MoneyOut
+             .where(money_date: start_month..Time.current.end_of_month)
+             .group(Arel.sql("DATE_TRUNC('month', money_date)::date"))
+             .order(Arel.sql("DATE_TRUNC('month', money_date)::date"))
+             .sum(:amount)
+
+    @most_expensive_money_outs = MoneyOut
+                                      .where(money_date: Time.current.beginning_of_month..Time.current.end_of_month)
+                                      .order(amount: :desc)
+                                      .limit(5)
+
+    @how_many_money_have = @money_in_in_current_month - @money_out_in_current_month
   end
 end
