cd4bf8cdd788cbfa86096583b824f3b7fa09ba58
NelcifranMagalhaes <nelcifranpires@gmail.com>
2026-03-16 16:29:50 -0300

    remove recurrency badge from money_out views and helper

--- app/helpers/money_outs_helper.rb
+++ app/helpers/money_outs_helper.rb
@@ -1,19 +1,11 @@
 module MoneyOutsHelper
-  def recurrency_badge(money_out)
-    if money_out.recurrency?
-      content_tag(:span, "Sim", class: "badge text-bg-success")
-    else
-      content_tag(:span, "Não", class: "badge text-bg-secondary")
-    end
-  end
-
   def installment_badge(money_out)
     if money_out.installment > 0 && money_out.original_money_out.present?
-      "#{money_out.installment}ª de #{money_out.original_money_out.try(:recurrency_quantity)}"
+      content_tag(:span, "#{money_out.installment}ª de #{money_out.original_money_out.try(:recurrency_quantity)}", class: "badge text-bg-info")
     elsif money_out.installment > 0 && money_out.original_money_out.nil?
-      "#{money_out.installment}ª de #{money_out.recurrency_quantity}"
+      content_tag(:span, "#{money_out.installment}ª de #{money_out.recurrency_quantity}", class: "badge text-bg-info")
     else
-      "Única"
+      content_tag(:span, "Única", class: "badge text-bg-secondary")
     end
   end
 end
